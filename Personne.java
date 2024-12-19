ceb11f59a3ec97786b6e3ed8fff909ccca18b9d4
hatimaderras <hatem.aderras@gmail.com>
2024-12-19 15:02:14 +0100

    creating personne classe

---
+++ Personne.java
@@ -0,0 +1,31 @@
+public class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+    private String email;
+
+
+    public Personne(int id, String nom, String prenom, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
